41b5cff4cd69798cfdf14285843d945ec2876c7e
Alvis Aung <alvisaung@gmail.com>
2023-05-02 18:10:49 +0630

    Initial Start

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} made on port ${PORT}`));
